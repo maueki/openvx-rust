f54d8b370832af4f7cf46fdb7b3f819f94c9af55
papamitra <papamitra@gmail.com>
2018-03-03 09:07:14 +0900

    Disable bindgen mangling

--- build.rs
+++ build.rs
@@ -15,6 +15,7 @@ fn main() {
         .blacklist_type("_vx_pixel_value_t")
         .constified_enum_module("vx_kernel_e")
         .constified_enum_module("vx_df_image_e")
+        .trust_clang_mangling(false)
         .generate()
         .expect("Unable to generate bindings");
 
