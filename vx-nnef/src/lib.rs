60a698564b4bf6fc208704abe4d14016bc9d269c
Ueki Masaru <masaru.ueki@itage.co.jp>
2018-03-20 19:46:09 +0900

    Add doc_to_graph() skelton

--- vx-nnef/src/lib.rs
+++ vx-nnef/src/lib.rs
@@ -1,3 +1,22 @@
+#![allow(dead_code)]
+
+extern crate vx;
+extern crate nnef_parser as np;
+
+#[macro_use]
+extern crate error_chain;
+
+error_chain! {
+    links {
+        Vx(vx::Error, vx::ErrorKind);
+    }
+}
+
+pub fn doc_to_graph<'a>(context: &'a vx::Context, _doc: &np::Document) -> Result<vx::Graph<'a>> {
+    Ok(context.create_graph()?)
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
